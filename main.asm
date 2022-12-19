232ef9df7f0c8b3c4fe4d511df29591df2dead65
Stephen Aranda <aranda.stephen88@gmail.com>
2022-12-18 20:28:16 -0600

    Added author comment to file

---
+++ main.asm
@@ -0,0 +1,6 @@
+@ Author: Stephen Aranda
+@ File Name: main.asm
+@ Date 12/18/2022
+@ Description: Running the race program that asks for three runner names and 
+@ times that each runner completed the race. Then displays who came in first,
+@ second, and third place.
